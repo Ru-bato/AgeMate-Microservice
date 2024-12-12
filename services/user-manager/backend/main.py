5d69d99f4a41899707f70c137804f591699b4e69
Ni Langen <2251334@tongji.edu.cn>
2024-12-12 15:06:08 +0800

    nle program preparation

--- services/user-manager/backend/main.py
+++ services/user-manager/backend/main.py
@@ -1,7 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# Developed by AlecNi @ 2024/12/12
+# Description:
+#   the backend program of the log-manager
+# Solved:
+# Unsolved:
+#   1.userInfo Struct
+#   2.login method
+#   3.regist method
+#   else
+
 from fastapi import FastAPI, Request
 import uvicorn
+import UserInfo
+import registManager
+import loginManager
 
 app = FastAPI()
+
+# TODO: import and invoke
 
 # 修改 user-manager 路由，接受 request 参数
 @app.get("/user-manager")
