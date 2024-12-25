7ae89c5cfee82c9d27d73f847e01c583c44bdf7e
ljw <2690500175@qq.com>
2024-12-25 21:29:22 +0800

    fronted framwork finish

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,21 +1,54 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import LoginRegisterView from '@/views/LoginRegisterView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import LoginRegisterView from '@/views/LoginRegisterView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'Home',
       component: HomeView,
+      children: [
+        {
+          path: '',
+          name: 'HomePage',
+          component: () => import('@/components/Home.vue')
+        },
+        {
+          path: 'tutorial-generate',
+          name: 'TutorialGenerate',
+          component: () => import('@/components/TutorialGenerate.vue'),
+        },
+        {
+          path: 'tutorial-execute',
+          name: 'TutorialExecute',
+          component: () => import('@/components/TutorialExecute.vue'),
+        },
+        {
+          path: 'log-management',
+          name: 'LogManagement',
+          component: () => import('@/components/LogManagement.vue'),
+        },
+        {
+          path: 'guideline-generate',
+          name: 'GuidelineGenerate',
+          component: () => import('@/components/GuidelineGenerate.vue'),
+        },
+        {
+          path: 'weather-entertainment',
+          name: 'WeatherEntertainment',
+          component: () => import('@/components/WeatherEntertainment.vue'),
+        },
+      ]
     },
     {
-      path: '/login',
+      path: '/',
       name: 'Login',
       component: LoginRegisterView,
     },
+    // 其他路由...
   ],
-})
+});
 
-export default router
+export default router;
